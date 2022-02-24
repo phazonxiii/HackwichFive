a2b40509384bfc26054c212e6f070af9e6c33dc4
Cliff Bailey <69611676+phazonxiii@users.noreply.github.com>
2022-02-24 13:23:16 -1000

    Problem Set One

--- HackwichFive/ViewController.swift
+++ HackwichFive/ViewController.swift
@@ -25,16 +25,23 @@ class ViewController: UIViewController {
         self.bottomLabel.text = favoriteFoodsArray[currentIndex]
     }
     @IBAction func buttonPressed(_ sender: Any) {
-        if self.currentIndex < self.favoriteFoodsArray.count
+        // so long as the current index is less than the total items in the array, the button will remain enabled
+        if self.currentIndex <
+            self.favoriteFoodsArray.count
         {
+            // Setting the name of the item into the bottom label starting from the current item
             self.bottomLabel.text = favoriteFoodsArray[currentIndex]
+            // Setting the text of the button to "NEXT"
             buttonLabel.setTitle("Next", for: UIControl.State.normal)
+            // Telling the label to change to the next item in the array when the button is pressed
             currentIndex += 1
         }
         else
         {
+            //
             print("button has been disabled")
-            (buttonLabel as! UIButton).isEnabled=false
+            //
+            (buttonLabel!).isEnabled=false
         }
     }
     
