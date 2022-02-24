113bb8f21c7026df09299a65fd3e1f7e9a7383a3
Cliff Bailey <69611676+phazonxiii@users.noreply.github.com>
2022-02-24 13:08:25 -1000

    Completed Parts 1-7

--- HackwichFive/ViewController.swift
+++ HackwichFive/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var topLabel: UILabel!
+    @IBOutlet var bottomLabel: UILabel!
+    
+    let currentIndex = 0
+    let favoriteFoodsArray = ["cheese pizza", "steak", "spaghetti and meatballs", "mochi", "ahi ahi poke"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.topLabel.text = "My favorite foods"
+        
+        self.bottomLabel.text = favoriteFoodsArray[currentIndex]
     }
-
+    @IBAction func buttonPressed(_ sender: Any) {
+    }
+    
+    
+    
 
 }
 
